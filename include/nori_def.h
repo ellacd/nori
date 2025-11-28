25aead6f00efa8e87d20cff650f9691ad3fc770e
Ella Duncan <ella.lr.duncan@gmail.com>
2025-11-27 22:34:28 -0800

    added 'experimental support' for using stdint.h fixed-width types if available

--- include/nori_def.h
+++ include/nori_def.h
@@ -8,6 +8,17 @@
 #define FALSE 0
 #endif
 
+#if __STDC_VERSION__ && __STDC_VERSION__ >= 199901L
+#include <stdint.h>
+typedef int8_t							int8;
+typedef uint8_t							uint8;
+typedef int16_t							int16;
+typedef uint16_t						uint16;
+typedef int32_t							int32;
+typedef uint32_t						uint32;
+typedef int64_t							int64;
+typedef uint64_t						uint64;
+#else
 typedef signed char 		    int8;
 typedef unsigned char 	    uint8;
 typedef signed short 		    int16;
@@ -16,6 +27,7 @@ typedef signed int 			    int32;
 typedef unsigned int 		    uint32;
 typedef signed long long 	  int64;
 typedef unsigned long long 	uint64;
+#endif
 typedef float 				      real32;
 typedef double 				      real64;
 typedef unsigned char 		  boolean;
