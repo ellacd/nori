53292b484ab041f920feb26a54385da94140e580
Ella Duncan <ella.lr.duncan@gmail.com>
2025-11-29 17:45:59 -0800

    Removed extraneous & unimplemented libraries

--- include/nori.h
+++ include/nori.h
@@ -1,6 +1,9 @@
 #ifndef NORI_H
 #define NORI_H
 
+#include <stdarg.h>
+#include <stddef.h>
+
 #ifndef NULL
 #define NULL	((void *)0)
 #endif
@@ -35,5 +38,37 @@ typedef double 				      real64;
 typedef unsigned char 		  boolean;
 
 #define ARRAY_LENGTH(x) (sizeof((x))/sizeof((x)[0]))
+
+// Useful data structures that I have to reimplement often
+#if !defined(NORI_DS)
+typedef void NList;
+#endif
+
+#define nlist(t) nlist_of_type(t)
+#define nlist_of_type(t) nlist_create((#t), sizeof(t));
+NList *	nlist_create(const char *name, uint16 stride);
+NList *	nlist_create_and_alloc(const char *name, uint16 stride, uint32 mlen);
+void    nlist_alloc(NList *l, uint32 mlen);
+void    nlist_destroy(NList *l);
+uint32 	nlist_len(const NList *l);
+boolean nlist_can_cast(const NList *to, const NList *from);
+NList *	nlist_copy(const NList *l);
+void    nlist_append(NList *onto, const NList *from);
+
+/*
+ * Accessors/mutators:
+ * NLists can pretend to be several data structures, including:
+ * - Stack (push()/pop(), alias to push_back()/pop_back())
+ * - Array/list (at(), insert())
+ * The programmer is trusted to differentiate between these themselves;
+ * no checks are made to restrict usage of these functions on any list.
+ */
+
+void *	nlist_at(const NList *l, uint32 p);
+void    nlist_insert(NList *l, uint32 p, const void *el);
+#define nlist_push(l,e) nlist_push_back((l),(e))
+void    nlist_push_back(NList *l, void *el);
+#define nlist_pop(l) nlist_pop_back((l))
+void *	nlist_pop_back(NList *l);
 
 #endif
---
+++ include/nori_internal.h
@@ -0,0 +1,6 @@
+#ifndef NORI_INTERNAL_H
+#define NORI_INTERNAL_H
+
+#include "nori.h"
+
+#endif
